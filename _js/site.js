fe4bcff0305db292eb01a8b6f1ac78f5fcc5bba1
Helen <helen@sigmaforge.com>
2015-09-12 12:44:51 +0100

    created initial files for website

---
+++ _js/site.js
@@ -0,0 +1,8 @@
+/*
+To link to a javascript file/s in the html page, in the head
+write:
+<script src="javascript.js"></script>
+<script src="navigation.js"></script>
+<script src="photo_gallery.js"></script>
+Note that each js file requires a separate reference.
+*/
